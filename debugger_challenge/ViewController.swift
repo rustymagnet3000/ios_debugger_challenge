c91c57c99e006665de39ac4e169d6b2739b2c926
rusty_magneto <rusty_magneto@openvpn-client.sslvpn.tbdevops.com>
2018-04-13 06:06:44 +0100

    Initial Commit

---
+++ debugger_challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  debugger_challenge
+//
+//  Created by rusty_magneto on 13/04/2018.
+//  Copyright © 2018 rusty_magneto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
