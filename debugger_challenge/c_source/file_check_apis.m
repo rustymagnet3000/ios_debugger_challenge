de72883c67fdc414d4bd7ce6994812d6c005f090
Rusty Robot <rustyrobot@robot.com>
2020-10-06 11:57:53 +0100

    Added call to C api Access via inline ASM

--- debugger_challenge/c_source/file_check_apis.m
+++ debugger_challenge/c_source/file_check_apis.m
@@ -3,6 +3,23 @@
 
 @implementation YDFileChecker
 
+//// int result = access(fp, F_OK);
+
+ +(uint64_t) asmSyscallFunction:(const char *) fp{
+    int64_t res = 99;
+    __asm (
+           "mov x0, #33\n" // access
+           "mov x1, %[input_path]\n" // copy char* to x1
+           "mov x2, #0\n"
+           "mov x16, #0\n"
+           "svc #33\n"
+           "mov %[result], x0 \n"
+    : [result] "=r" (res)
+    : [input_path] "r" (fp)
+    : "x0", "x1", "x2", "x16", "memory"
+    );
+    return res;
+}
 
 +(BOOL)checkFileExists{
     
@@ -11,25 +28,21 @@
     NSString *filepath = [appbundle pathForResource:@"Info" ofType:@"plist"];
     const char *fp = filepath.fileSystemRepresentation;
     
-    /* SIGSYS means call was wrong !*/
-    
     #if defined(__arm64__)
-        NSLog(@"access for __arm64__");
-
-        __asm(
-            "mov     x0, x0\n\t"
-            "mov     x0, x0"
-        );
-    
+        NSLog(@"access for __arm64__ with ASM instructions");
+        int64_t result = [self asmSyscallFunction:fp];
+        NSLog(@"Result:%lld", result);
         file_exists = YES;
+    
     #elif defined(__x86_64__)
         NSLog(@"access for __x86_64__");
         int result = access(fp, F_OK);
         printf("[*] result: %d\n", result);
         file_exists == 0 ? YES: NO;
+    
     #else
         NSLog(@"Unknown target.");
-        file_exists NO;
+        file_exists = NO;
     #endif
     return file_exists;
 }
